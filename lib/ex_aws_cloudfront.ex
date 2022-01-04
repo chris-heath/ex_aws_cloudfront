2130c9c41e0cc373196aa5ce073920fc35bfa0da
Justin Tang <justin.tang@vonage.com>
2022-01-04 15:42:06 -0500

    initial commit

---
+++ lib/ex_aws_cloudfront.ex
@@ -0,0 +1,18 @@
+defmodule ExAwsCloudfront do
+  @moduledoc """
+  Documentation for `ExAwsCloudfront`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExAwsCloudfront.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_aws_cloudfront_test.exs
@@ -0,0 +1,8 @@
+defmodule ExAwsCloudfrontTest do
+  use ExUnit.Case
+  doctest ExAwsCloudfront
+
+  test "greets the world" do
+    assert ExAwsCloudfront.hello() == :world
+  end
+end
